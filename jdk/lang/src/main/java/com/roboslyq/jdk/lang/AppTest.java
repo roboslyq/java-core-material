87ff4b183b7a73cdb159a00549a0cf26ec405ad0
roboslyq <robosloveu@163.com>
2019-03-28 14:36:58 +0800

    Sort

---
+++ jdk/lang/src/main/java/com/roboslyq/jdk/lang/AppTest.java
@@ -0,0 +1,15 @@
+package com.roboslyq.jdk.lang;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public static void main(String[] args) {
+        List<Integer> a =  new ArrayList<>();
+    }
+}
