22db4246a3951646712fe2e023cb64b99330ac55
roboslyq <robosloveu@163.com>
2019-07-09 15:08:33 +0800

    updated

--- core/src/test/java/com/roboslyq/core/TestMain1.java
+++ core/src/test/java/com/roboslyq/core/TestMain1.java
@@ -17,14 +17,44 @@ import java.util.Collections;
 /**
  *
  * 〈通用测试类使用〉
+ * 负数使用补码表示，
+ * 补码 = 反码 + 1;
  * @author luo.yongqian
  * @create 2019/4/19
  * @since 1.0.0
  */
 public class TestMain1 {
     public static void main(String[] args) {
-        long estimate = 10;
-        System.out.println(estimate >>>= 1);
+//
+//        final int start = Integer.MAX_VALUE - 100;
+//        final int end = Integer.MAX_VALUE;
+//        int count = 0;
+//        for (int i = start; i <= end; i++)
+//            count++;
+//        System.out.println("count-->" + count);
+//        Integer temp = Integer.MAX_VALUE;
+//        Integer temp2 = Integer.MAX_VALUE + 1;
+//        System.out.println(Integer.MAX_VALUE + 1);
+        //正整数1，2，3
+        Integer  temp1 = 0b00000000_00000000_00000000_00000001;
+        Integer  temp2 = 0b00000000_00000000_00000000_00000010;
+        Integer  temp3 = 0b00000000_00000000_00000000_00000011;
+        System.out.println(temp1);
+        System.out.println(temp2);
+        System.out.println(temp3);
+        //正整数1，2，3
+        Integer  temp4 = 0b10000000_00000000_00000000_00000001;
+        Integer  temp4_1 = 0b10000000_00000000_00000000_00000000;
+        Integer  temp5 = 0b10000000_11111111_00000000_00000010;
+        Integer  temp6 = 0b11111111_11111111_11111111_11111011;
+        Integer temp7 = -5;
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(temp4);
+        System.out.println(temp4_1);
+        System.out.println(temp5);
+        System.out.println(temp6);
+//        long estimate = 10;
+//        System.out.println(estimate >>>= 1);
 //        System.out.println(1 << 30);
 //        System.out.println(1 << 6 );
 //        JSONArray jsonArray = new JSONArray();
