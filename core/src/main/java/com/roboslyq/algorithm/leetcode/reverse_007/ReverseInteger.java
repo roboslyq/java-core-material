e65ab744e2cbd2043047f76c9248a464e22646a0
roboslyq <robosloveu@163.com>
2019-07-22 23:41:52 +0800

    leetcode 学习测试

---
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/reverse_007/ReverseInteger.java
@@ -0,0 +1,21 @@
+package com.roboslyq.algorithm.leetcode.reverse_007;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        System.out.println( reverseInteger.reverse(-12345));
+    }
+    public int reverse(int x) {
+        int flag = x < 0 ? -1 : 1;
+        int tmp = Math.abs(x);
+        int result  = 0;
+        int tmp2 = Integer.MAX_VALUE/10;
+        while (tmp > 0){
+            int remainder = tmp%10;
+            if(result > tmp2 ) return 0;
+            result = result * 10 + remainder ;
+            tmp = tmp/10;
+        }
+        return flag * result;
+    }
+}
