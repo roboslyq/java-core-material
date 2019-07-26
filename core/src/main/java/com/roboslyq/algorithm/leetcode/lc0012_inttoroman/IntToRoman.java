b53480c38347aec026de92edf4c2b63d56275aaa
roboslyq <robosloveu@163.com>
2019-07-26 22:24:35 +0800

    leetcode 学习测试

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0012_inttoroman/IntToRoman.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0012_inttoroman/IntToRoman.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 /**
  *
- * 〈〉
+ * 〈映射法：每个位的情况共有，使用Map保存结果，然后根据每一位映射〉
  * @author luo.yongqian
  * @create 2019/7/26
  * https://leetcode-cn.com/problems/integer-to-roman/
@@ -23,30 +23,58 @@ import java.util.Map;
  */
 public class IntToRoman {
     public static void main(String[] args) {
-
+        IntToRoman intToRoman = new IntToRoman();
+//        System.out.println(intToRoman.intToRoman(3));
+//        System.out.println(intToRoman.intToRoman(4));
+//        System.out.println(intToRoman.intToRoman(9));
+//        System.out.println(intToRoman.intToRoman(58));
+        System.out.println(intToRoman.intToRoman(1994));
     }
+
     public String intToRoman(int num) {
-        Map<Integer,String> romanMap = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        String result = "";
-        romanMap.put(1,"I");
-        romanMap.put(2,"II");
-        romanMap.put(3,"III");
-        romanMap.put(4,"IV");
-        romanMap.put(5,"V");
-        romanMap.put(6,"VI");
-        romanMap.put(7,"VII");
-        romanMap.put(8,"VIII");
-        romanMap.put(9,"IX");
-        romanMap.put(10,"X");
-        romanMap.put(100,"C");
-        romanMap.put(500,"D");
-        romanMap.put(1000,"M");
-        int bit = 10;
-        while(num > 0){
-            int mode = num % bit;
-            result
-        }
+         Map<Integer,String> romanMap = new HashMap<>();
+            romanMap.put(0,"");
+            romanMap.put(1,"I");
+            romanMap.put(2,"II");
+            romanMap.put(3,"III");
+            romanMap.put(4,"IV");
+            romanMap.put(5,"V");
+            romanMap.put(6,"VI");
+            romanMap.put(7,"VII");
+            romanMap.put(8,"VIII");
+            romanMap.put(9,"IX");
+            romanMap.put(10,"X");
+            romanMap.put(20,"XX");
+            romanMap.put(30,"XXX");
+            romanMap.put(40,"XL");
+            romanMap.put(50,"L");
+            romanMap.put(60,"LX");
+            romanMap.put(70,"LXX");
+            romanMap.put(80,"LXXX");
+            romanMap.put(90,"XC");
+            romanMap.put(100,"C");
+            romanMap.put(200,"CC");
+            romanMap.put(300,"CCC");
+            romanMap.put(400,"CD");
+            romanMap.put(500,"D");
+            romanMap.put(600,"DC");
+            romanMap.put(700,"DCC");
+            romanMap.put(800,"DCCC");
+            romanMap.put(900,"CM");
+            romanMap.put(1000,"M");
+            romanMap.put(2000,"MM");
+            romanMap.put(3000,"MMM");
+        String res = "";
 
+        int bit = 10;
+        int mutiple = 1;
+        while(num * 10/bit > 0){
+            int mode = num % 10;
+            res = String.format("%s%s", romanMap.get(mode * mutiple), res);
+            num = num /10;
+            mutiple *= 10;
+        }
+        return  res;
     }
+
 }
