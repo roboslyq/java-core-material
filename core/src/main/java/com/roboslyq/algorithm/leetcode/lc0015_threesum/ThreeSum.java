fcee6eb54e2314ab657c438aadd947304a6afdda
roboslyq <robosloveu@163.com>
2019-07-29 23:22:43 +0800

    leetcode 学习测试

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0015_threesum/ThreeSum.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0015_threesum/ThreeSum.java
@@ -9,7 +9,8 @@ import java.util.*;
 public class ThreeSum {
     public static void main(String[] args) {
         ThreeSum threeSum = new ThreeSum();
-        List<List<Integer>> res = threeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+//        List<List<Integer>> res = threeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        List<List<Integer>> res = threeSum.threeSum1(new int[]{-1, 0, 1, 2, -1, -4});
         for(List<Integer>  listInt : res ){
             for(Integer integer : listInt){
                 System.out.print(integer + " ");
@@ -17,6 +18,32 @@ public class ThreeSum {
             System.out.println();
         }
     }
+    public  List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        Arrays.sort(nums); // 排序
+        int len = nums.length;
+        if(nums == null || len < 3) return ans;
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+            int L = i+1;
+            int R = len-1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
+        }
+        return ans;
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> resultList = new ArrayList<>();
         Arrays.sort(nums);
---
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0016_3sumclosest/ThreeSumClosest.java
@@ -0,0 +1,41 @@
+package com.roboslyq.algorithm.leetcode.lc0016_3sumclosest;
+
+import java.util.Arrays;
+
+/**
+ * 暴力法
+ */
+public class ThreeSumClosest {
+    public static void main(String[] args) {
+        ThreeSumClosest threeSumClosest = new ThreeSumClosest();
+        int res = threeSumClosest.threeSumClosest(new int[]{-1,2,1,-4},1);
+            System.out.println(res);
+    }
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = 0;
+        for(int i = 0 ;i< nums.length -2 ;i++){
+            int vali = nums[i];
+            for(int j = i+1;j<nums.length-1;j++){
+                int next1 = nums[j];
+                for(int k = j + 1;k < nums.length ;k++){
+                    int next2 = nums[k];
+                    int resTmp = vali + next1 + next2;
+                    if(resTmp == target ){
+                        return target;
+                    }else{
+                        if(i == 0 && j==1 && k==2) {
+                            result = resTmp ;
+                        }
+                        else{
+                            if (Math.abs(resTmp -target ) < Math.abs(result - target) ){
+                                result = resTmp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
