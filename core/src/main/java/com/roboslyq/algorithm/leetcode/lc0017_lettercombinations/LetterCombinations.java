658f55f34b7a435f93de7e8d317fcdeca651aa19
roboslyq <robosloveu@163.com>
2019-07-31 00:06:24 +0800

    leetcode 学习测试

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0017_lettercombinations/LetterCombinations.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0017_lettercombinations/LetterCombinations.java
@@ -11,6 +11,7 @@
 package com.roboslyq.algorithm.leetcode.lc0017_lettercombinations;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -19,16 +20,41 @@ import java.util.List;
  * @author luo.yongqian
  * @create 2019/7/30
  * @since 1.0.0
+ * 难点是如果保存数据
  */
 public class LetterCombinations {
     public static void main(String[] args) {
-
+        LetterCombinations letterCombinations = new LetterCombinations();
+        List<String> res = letterCombinations.letterCombinations("234");
+        System.out.println("res.size()"+res.size());
+        for(String str : res){
+            System.out.println(str);
+        }
     }
     public List<String> letterCombinations(String digits) {
-        List<String> resList = new ArrayList<>();
+         String[] mapNum = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> resultCurrent = new LinkedList<>();
+        List<String> resultPutInto  = new LinkedList<>();
         for(int i = 0;i< digits.length();i++){
-
+            if(i>1){
+                resultCurrent.clear();//释放空间
+                resultCurrent = resultPutInto;
+                resultPutInto = new LinkedList<>();
+;            }
+            int index = Integer.valueOf(digits.charAt(i)+"");
+            String currentStr = mapNum[index];
+            if(resultCurrent.size() == 0){
+                for(int j=0;j<currentStr.length();j++){
+                    resultCurrent.add(currentStr.charAt(j)+"");
+                }
+            }else{
+                for(String resTmp : resultCurrent){
+                    for(int j=0;j<currentStr.length();j++){
+                        resultPutInto.add(resTmp + currentStr.charAt(j));
+                    }
+                }
+            }
         }
-
+        return  resultPutInto.size() > resultCurrent.size() ? resultPutInto : resultCurrent;
     }
 }
---
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0017_lettercombinations/LetterCombinations2.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C), 2015-2019
+ * FileName: LetterCombinations
+ * Author:   luo.yongqian
+ * Date:     2019/7/30 18:07
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2019/7/30 18:07      1.0.0               创建
+ */
+package com.roboslyq.algorithm.leetcode.lc0017_lettercombinations;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * 〈〉
+ * @author luo.yongqian
+ * @create 2019/7/30
+ * @since 1.0.0
+ * 递归思想
+ */
+public class LetterCombinations2 {
+    public static void main(String[] args) {
+        LetterCombinations2 letterCombinations = new LetterCombinations2();
+        List<String> res = letterCombinations.letterCombinations("234");
+        System.out.println("res.size()"+res.size());
+        for(String str : res){
+            System.out.println(str);
+        }
+    }
+  private String letterMap[] = {
+            " ",    //0
+            "",     //1
+            "abc",  //2
+            "def",  //3
+            "ghi",  //4
+            "jkl",  //5
+            "mno",  //6
+            "pqrs", //7
+            "tuv",  //8
+            "wxyz"  //9
+    };
+
+    private ArrayList<String> res;
+
+    public List<String> letterCombinations(String digits) {
+
+        res = new ArrayList<String>();
+        if(digits.equals(""))
+            return res;
+
+        findCombination(digits, 0, "");
+        return res;
+    }
+
+    private void findCombination(String digits, int index, String s){
+
+        if(index == digits.length()){
+            res.add(s);
+            return;
+        }
+
+        Character c = digits.charAt(index);
+        String letters = letterMap[c - '0'];
+        for(int i = 0 ; i < letters.length() ; i ++){
+            findCombination(digits, index+1, s + letters.charAt(i));
+        }
+
+        return;
+    }
+}
