6b5a712da95725a903bf3963c5a03bc2671bdf60
roboslyq <robosloveu@163.com>
2019-07-30 18:09:18 +0800

    updated

---
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0017_lettercombinations/LetterCombinations.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C), 2015-2019
+ * FileName: LetterCombinations
+ * Author:   luo.yongqian
+ * Date:     2019/7/30 18:07
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2019/7/30 18:07      1.0.0               创建
+ */
+package com.roboslyq.algorithm.leetcode.lc0017_lettercombinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 〈〉
+ * @author luo.yongqian
+ * @create 2019/7/30
+ * @since 1.0.0
+ */
+public class LetterCombinations {
+    public static void main(String[] args) {
+
+    }
+    public List<String> letterCombinations(String digits) {
+        List<String> resList = new ArrayList<>();
+        for(int i = 0;i< digits.length();i++){
+
+        }
+
+    }
+}
