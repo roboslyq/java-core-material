d502f361e89c468d6920d33bf5e19bfe1e4ece2c
roboslyq <robosloveu@163.com>
2019-08-01 18:03:44 +0800

    updated

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0019_removenthfromend/RemoveNthFromEnd.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0019_removenthfromend/RemoveNthFromEnd.java
@@ -10,6 +10,9 @@
  */
 package com.roboslyq.algorithm.leetcode.lc0019_removenthfromend;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * 〈〉
@@ -36,5 +39,26 @@ package com.roboslyq.algorithm.leetcode.lc0019_removenthfromend;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class RemoveNthFromEnd {
+    public static void main(String[] args) {
 
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        Map<Integer,ListNode> iListNode = new HashMap<>();
+        ListNode tmp = head;
+        int i = 0;
+        while(tmp != null){
+            iListNode.put(i,tmp);
+            tmp = tmp.next;
+            i++;
+        }
+        if(n >)
+       return head;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
