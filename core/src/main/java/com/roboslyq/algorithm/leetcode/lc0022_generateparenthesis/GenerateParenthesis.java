c78f05f8adbd78b9bfe0a8442e81ed567f16c81c
roboslyq <robosloveu@163.com>
2019-08-05 14:21:50 +0800

    updated

---
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0022_generateparenthesis/GenerateParenthesis.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C), 2015-2019
+ * FileName: GenerateParenthesis
+ * Author:   luo.yongqian
+ * Date:     2019/8/5 13:04
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2019/8/5 13:04      1.0.0               创建
+ */
+package com.roboslyq.algorithm.leetcode.lc0022_generateparenthesis;
+
+import java.util.List;
+
+/**
+ *
+ * 〈为了枚举某些内容，我们通常希望将其表示为更容易计算的不相交子集的总和。〉
+ * @author luo.yongqian
+ * @create 2019/8/5
+ * @since 1.0.0
+ */
+public class GenerateParenthesis {
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> generateParenthesis(int n) {
+
+    }
+
+}
