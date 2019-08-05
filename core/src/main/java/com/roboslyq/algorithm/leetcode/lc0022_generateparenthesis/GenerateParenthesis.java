77eee702bb8bba06e8b1efcf963c7f3069aabff9
roboslyq <robosloveu@163.com>
2019-08-05 18:30:42 +0800

    updated

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0022_generateparenthesis/GenerateParenthesis.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0022_generateparenthesis/GenerateParenthesis.java
@@ -10,6 +10,7 @@
  */
 package com.roboslyq.algorithm.leetcode.lc0022_generateparenthesis;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,11 +22,39 @@ import java.util.List;
  */
 public class GenerateParenthesis {
     public static void main(String[] args) {
-
+        GenerateParenthesis generateParenthesis = new GenerateParenthesis();
+        List<String> res = generateParenthesis.generateParenthesis(3);
+        for(String str : res){
+            System.out.println(str);
+        }
     }
 
     public List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
 
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current))
+                result.add(new String(current));
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos+1, result);
+            current[pos] = ')';
+            generateAll(current, pos+1, result);
+        }
+    }
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c: current) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return (balance == 0);
     }
 
 }
