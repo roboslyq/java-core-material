53b7cbedda46d1bfee2888025b59ecf21936106b
roboslyq <robosloveu@163.com>
2019-08-12 23:50:01 +0800

    gc 学习测试

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java
@@ -9,8 +9,36 @@ import java.util.Stack;
  */
 public class ReverseKGroup {
     public static void main(String[] args) {
+        ReverseKGroup reverseKGroup = new ReverseKGroup();
+        ListNode listNode1 = new ListNode(1);
 
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        ListNode res = reverseKGroup.reverse(listNode1);
+        while (res != null)
+        {
+            System.out.println(res.val);
+            res = res.next;
+        }
     }
+    private ListNode reverse(ListNode head) {
+        ListNode curResult = null;//保存翻转后结果ListNode的head元素
+        ListNode curr = head;//当前待翻转的元素，即翻转前的head元素
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = curResult;
+            curResult = curr;
+            curr = next;
+        }
+        return curResult;
+    }
+
     public ListNode reverseKGroup(ListNode head, int k) {
         Stack<ListNode> stack = new Stack<>();
         ListNode resHead = null;
