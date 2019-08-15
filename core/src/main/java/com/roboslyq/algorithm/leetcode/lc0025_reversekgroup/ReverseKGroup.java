4c25a801717ec5078ca596d48cdf23545d94913f
roboslyq <robosloveu@163.com>
2019-08-15 13:31:30 +0800

    updated

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java
@@ -14,52 +14,54 @@ public class ReverseKGroup {
         ListNode l2 = new ListNode(2);
         ListNode l3 = new ListNode(3);
         ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
         l1.next = l2;
         l2.next = l3;
         l3.next = l4;
-        ListNode reverse = rk.reverse(l1);
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = l7;
+        l7.next = l8;
+        ListNode reverse = rk.reverseKGroup(l1,3);
         while (reverse != null){
             System.out.println(reverse.val);
             reverse = reverse.next;
         }
 
     }
-    public ListNode reverse(ListNode listNode,int k){
-        ListNode resHead = null;
-        ListNode curr = listNode;
-        int j = 0;
-        while (curr != null){
-            //临时保存下一个将要处理的元素
-            ListNode currNext = curr.next;
-            //将之前结果作为当前元素的next元素
-            curr.next = resHead;
-            //将处理后的节点保存到结果中
-            resHead = curr;
-            //切换下一个要处理的元素为当前元素
-            curr = currNext;
-            j++;
-            if(j == k ){
-                break;
-            }
-        }
-        return resHead;
-    }
-    public ListNode reverseKGroup(ListNode head, int k) {
-        Stack<ListNode> stack = new Stack<>();
-        ListNode resHead = null;
-        ListNode curReverseHead = head;
-        int i = 0;
-        while(true){
-            if(i < k - 1){//入栈，
-                stack.push(head);
-                head = head.next;
-                i++;
-            }else{
-                head = head.next;
-                ListNode headNew = stack.pop();
 
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode resHead = null;
+        ListNode resTail = null;
+        ListNode curr = head;
+        int j = 0;//已翻转元素的个数处理
+        if(curr != null){
+            while (j != k && curr != null ) {
+                if(j==0){
+                    resTail = curr;
+                }
+                //临时保存下一个将要处理的元素
+                ListNode currNext = curr.next;
+                //将之前结果作为当前元素的next元素
+                curr.next = resHead;
+                //将处理后的节点保存到结果中
+                resHead = curr;
+                //切换下一个要处理的元素为当前元素
+                curr = currNext;
+                j++;
+            }
+            if(j == k){
+                resTail.next = reverseKGroup(curr,k);
+            }else{
+                //元素不够，重新反转回来
+                resTail = reverseKGroup(curr,j);
             }
         }
+
+        return resHead;
 
     }
 }
