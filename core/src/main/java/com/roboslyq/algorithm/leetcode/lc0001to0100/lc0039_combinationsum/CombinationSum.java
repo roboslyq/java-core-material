460cc6bda8d0273fb0eec4fda615d7ea5e82d080
roboslyq <robosloveu@163.com>
2019-09-08 10:41:06 +0800

    Merge branch 'master' of https://github.com/roboslyq/java-core-material
    # Conflicts:
    #	core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0001to0100/lc0039_combinationsum/CombinationSum.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0001to0100/lc0039_combinationsum/CombinationSum.java
@@ -16,16 +16,43 @@ import java.util.List;
 
 /**
  *
- * 〈〉
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * candidates 中的数字可以无限制重复被选取。
+ * 说明：
+ * 所有数字（包括 target）都是正整数。
+ * 解集不能包含重复的组合。 
+ * 示例 1:
+ * 输入: candidates = [2,3,6,7], target = 7,
+ * 所求解集为:
+ * [
+ *   [7],
+ *   [2,2,3]
+ * ]
+ * 示例 2:
+ *
+ * 输入: candidates = [2,3,5], target = 8,
+ * 所求解集为:
+ * [
+ *   [2,2,2,2],
+ *   [2,3,3],
+ *   [3,5]
+ * ]
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combination-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @author luo.yongqian
  * @create 2019/8/29
  * @since 1.0.0
  */
+
+/**
+ * 题解：使用加法实现(回溯+剪枝)
+ */
 public class CombinationSum {
     public static void main(String[] args) {
         CombinationSum combinationSum = new CombinationSum();
-        int [] input = {2,3,6,7};
-        int target = 7;
+        int [] input = {2,3,5};
+        int target = 8;
         List<List<Integer>>  res =  combinationSum.combinationSum(input,target);
         for(List<Integer> list : res){
             for(Integer tmp : list){
@@ -34,147 +61,46 @@ public class CombinationSum {
             System.out.println();
         }
     }
+
+    //进行排序(类变量，不需要传参)
+    //包含static 的leetcode会报错
+//    public static List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> res = new ArrayList<>();//
+    //进行排序(类变量，不需要传参)
+    public Integer target = 0;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        //进行排序
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(candidates);
-        if(candidates[0] > target){
-            return res;
-        }
-        int rightMax = getMiddleIndex(candidates, target);
-        for(int i = 1;i<= rightMax;i++){
-          List<List<Integer>> resTmp =  kSum(candidates, target, i);
-          res.addAll(resTmp);
-        }
+        //入口排序
+        //Arrays.sort(candidates);
+//        if(candidates[0] > target){
+//            return res;
+//        }
+        this.target = target;
+        List<Integer> list = new ArrayList<>();
+        recursive(candidates,list,0,0);
         return res;
     }
 
-    public int getMiddleIndex(int[] candidates,int target){
-
-        int left = 0;
-        int right = candidates.length -1;
-        int index = right;
-        while (left <= right){
-            int middle = (left + right)/2;
-            if(middle == target){
-                index = middle;
-            }
-            if(middle > target){
-                right = middle - 1;
-            }
-            if(middle < target){
-                left = middle + 1;
-            }
-        }
-        return index;
-    }
-
-
     /**
-     * 我是一个接口，在系统提供的他们的方法里面调用我即可
      *
-     * 相当加了一层包装，对外提供了一个系统可以使用的接口
-     * @param nums 系统给定的数组
-     * @param target 系统要求的目标值
-     * @return 系统要求的返回值
+     * @param candidates 原数组
+     * @param currentList 当前list
+     * @param currentValue 当前值，不需要当前List计算
+     * @param index 当前索引位轩，默认从0开始
      */
-    public List<List<Integer>> kSum(int[] nums, int target, int k) {
-        // 先排序，这个是必须的。
-        //Arrays.sort(nums);
-
-        // 根据模板方法的要求，将该方法需要的输入都准备好。
-        int[] stack = new int[k];
-        Arrays.fill(stack, 0x3f3f3f3f);
-        int stack_index = -1;
-        int begin = 0;
-        // 递归开始
-        List<List<Integer>> ans = K_Sum_Recursive_Template(nums, stack, stack_index, k, begin, target);
-        // 递归结束，返回解
-        return ans;
-    }
-
-    /**
-     * K数之和问题的模板方法，所有K数问题都可以调用这个方法
-     * @param nums 输入的数组
-     * @param stack 定义的一个长度为 k_sum 问题中的 k 的数组，初始化为0x3f3f3f3f
-     * @param stack_index 栈指针，初始化值为-1
-     * @param k 表明当前问题被 分解/递归 成了 k数之和 的问题
-     * @param begin 当前问题要固定的值的起点
-     * @param target 当前 k数之和 的目标和
-     * @return 当前 k数之和 的解集，要在上一层合并到最终解集里去
-     */
-    private List<List<Integer>> K_Sum_Recursive_Template(int[] nums, int[] stack, int stack_index, int k, int begin, int target){
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // 当递归到两数之和的时候，不再进行递归，直接使用左右指针法解决
-        if(k == 2){
-            List<Integer> temp_ans;
-
-            int left = begin;
-            int right = nums.length - 1;
-
-            while(left < right){
-                if(nums[left] + nums[right] > target){
-                    // 过大，因此右指针左移
-                    right--;
-                }else if(nums[left] + nums[right] < target){
-                    // 过小，因此左指针右移
-                    left++;
-                }else {
-                    // 相等，加入序列中，左右指针同时向内移动一次
-                    temp_ans = new ArrayList<>();
-                    stack[++stack_index] = nums[left];
-                    stack[++stack_index] = nums[right];
-
-                    // 当前栈中的元素符合题目要求， 将其加入到List中去，并将该List加入到当前问题的解集中
-                    for(int i = 0; i <= stack_index; i++){
-                        temp_ans.add(stack[i]);
-                    }
-                    ans.add(temp_ans);
-
-                    // 栈的清理工作，其实不做也可以，因为栈指针属于形参，不会影响外面的那个栈指针，
-                    // 但是还是清理掉比较好，方便调试。
-                    stack[stack_index--] = 0x3f3f3f3f;
-                    stack[stack_index--] = 0x3f3f3f3f;
-
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]){
-                        left++;
-                    }
-                    while (right > left && right < nums.length - 1 && nums[right] == nums[right + 1]){
-                        right--;
-                    }
-                }
-            }
-        }else {
-            int target_temp;
-            for(int i = begin; i < nums.length - k + 1; i++){
-                if(i > begin && nums[i] == nums[i - 1]){
-                    continue;
-                }
-                // 在固定一个数后，问题被降级为一个 k - 1 数之和 问题
-                // 确定 k - 1 数之和 的目标和
-                target_temp = target - nums[i];
-                // 将当前选定的数字压入栈中，便于最后加入解集中
-                stack[++stack_index] = nums[i];
-                // 递归调用 k - 1 数之和 问题的求解
-                List<List<Integer>> ans_temp = K_Sum_Recursive_Template(nums,stack, stack_index, k - 1, i + 1, target_temp);
-                // 在选定当前数字的情况下， k - 1 数之和 问题求解完毕，
-                // 将该数弹出栈，为选择下一个被选值做准备
-                stack[stack_index--] = 0x3f3f3f3f;
-                // 将当前解集加入当前 k数之和的解集中
-                ans.addAll(ans_temp);
-            }
+    public void recursive(int[] candidates, List<Integer> currentList,int currentValue,int index){
+        if(currentValue > target){
+            return;
         }
-        return ans;
+        if(currentValue == target){
+            res.add(currentList);
+            return;
+        }
+        for(int i=index;i<candidates.length;i++){
+            List<Integer> tmpList = new ArrayList<>(currentList);
+            int currentNew = currentValue + candidates[i];
+            tmpList.add(candidates[i]);
+            //递归回溯(注意：i不变，保证下次再包含当前位置元素）
+            recursive(candidates, tmpList, currentNew,i);
+        }
     }
-    public static void test(){
-        CombinationSum solution = new CombinationSum();
-        int [] input = {0,0,0,0};
-        int k = 4;
-        int target = 0;
-        solution.kSum(input, target, k);
-    }
-
 }
