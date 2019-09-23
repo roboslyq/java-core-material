9a383fb173b2c9ebc160f16b59202285a5ae205f
roboslyq <robosloveu@163.com>
2019-09-24 00:10:50 +0800

    Merge branch 'master' of https://github.com/roboslyq/java-core-material
    # Conflicts:
    #	core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java

--- core/src/main/java/com/roboslyq/algorithm/leetcode/lc0001to0100/lc0092_reversebetween/ReverseBetween.java
+++ core/src/main/java/com/roboslyq/algorithm/leetcode/lc0001to0100/lc0092_reversebetween/ReverseBetween.java
@@ -14,40 +14,47 @@ public class ReverseBetween {
         l3.next = l4;
         l4.next = l5;
         ReverseBetween reverseBetween = new ReverseBetween();
-        reverseBetween.reverseBetween(l1,2,4);
-        while (l1.next != null){
+        reverseBetween.reverseBetween(l1,1,5);
+        while (l1 != null){
             System.out.println(l1.val);
             l1 = l1.next;
         }
     }
     public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(m == n ) return head;
         ListNode before_start_reverse = null;
-        ListNode after_start_reverse = null;
         ListNode start_reverse = null;
         ListNode pre = null;//反转链条的前置节点
-//        ListNode after_end_erverse=null;
-//        ListNode after_end_rerverse=null;
-        ListNode end_reverse = null;
         ListNode cur = head; //初始化cur,即相当于下面for循环的i=1
         ListNode tmp = null;
-        for (int i = 1; i < Integer.MAX_VALUE ; i++) {
+        for (int i = 0; i < Integer.MAX_VALUE ; i++) {
             if(i == m-1){//反转前处理(超始位置要取前，因为需要保存前一节点)
-                before_start_reverse = cur;
-                start_reverse = cur.next;
+                if(m==1){
+                    start_reverse = cur;
+                }
+                else{
+                    start_reverse = cur.next;
+                }
                 pre = start_reverse;
+                before_start_reverse = cur;
+                continue;
             }
-            if(i>= m && i <= n){//反转中处理
+            if(i>= m && i < n){//反转中处理
                 tmp = cur.next;
                 cur.next = pre;
                 pre = cur;
                 cur = tmp;
-
+                continue;
             }
-            if(i == n+1) {//反转结尾处理
+            if(i == n) {//反转结尾处理
+                tmp = cur.next;
+                cur.next = pre;
                 before_start_reverse.next = cur;
+                start_reverse.next = tmp;
 
                 break;
             }
+            cur = cur.next;
         }
         return head;
     }
