9239ad2a74e61095a5f379fe2f8279209eb872f1
roboslyq <robosloveu@163.com>
2019-09-25 23:04:06 +0800

    Merge branch 'master' of https://github.com/roboslyq/java-core-material
    # Conflicts:
    #	core/src/main/java/com/roboslyq/algorithm/leetcode/lc0025_reversekgroup/ReverseKGroup.java

--- core/src/main/java/com/roboslyq/datastruct/tree/iterator/LevelOrder.java
+++ core/src/main/java/com/roboslyq/datastruct/tree/iterator/LevelOrder.java
@@ -10,13 +10,39 @@
  */
 package com.roboslyq.datastruct.tree.iterator;
 
+import com.roboslyq.datastruct.tree.BinaryTreeNode;
+import com.roboslyq.datastruct.tree.BinaryTreeUtil;
+
+import java.util.LinkedList;
+
 /**
  *
- * 〈层遍列〉
+ * 〈层遍列 BSF breadth search firstly〉
  * @author luo.yongqian
  * @create 2019/9/25
  * @since 1.0.0
  */
 public class LevelOrder {
+    public static void main(String[] args) {
+        BinaryTreeNode headNode = BinaryTreeUtil.generate1(4);
+        LevelOrder levelOrder = new LevelOrder();
+        levelOrder.levelOrder(headNode);
+    }
+
+    /**
+     * 迭代完成
+     * @param treeNode
+     */
+    public void levelOrder(BinaryTreeNode treeNode){
+        LinkedList<BinaryTreeNode> linkedList = new LinkedList<>();
+        linkedList.push(treeNode);
+        BinaryTreeNode cur;
+        while (!linkedList.isEmpty()){
+            cur = linkedList.pop();
+            System.out.println(cur.getT());
+            if(cur.getLeftNode() != null) linkedList.addLast(cur.getLeftNode());
+            if(cur.getRightNode() != null) linkedList.addLast(cur.getRightNode());
+        }
+    }
 
 }
