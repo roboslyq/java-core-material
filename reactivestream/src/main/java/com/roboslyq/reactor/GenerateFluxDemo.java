5ca35089b7a3162fa57273b25b9e35de2cebf08b
roboslyq <robosloveu@163.com>
2020-03-05 16:53:10 +0800

    updated

--- reactivestream/src/main/java/com/roboslyq/reactor/GenerateFluxDemo.java
+++ reactivestream/src/main/java/com/roboslyq/reactor/GenerateFluxDemo.java
@@ -15,22 +15,49 @@ public class GenerateFluxDemo {
      * 通过 Flux 类的静态方法创建 Flux 序列
      */
     public static void simpleFluxByStaticMethod(){
-        Flux.just("Hello","World").subscribe(System.out::println);
-        Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(System.out::println);
-        Flux.empty().subscribe(System.out::println);
-        Flux.range(1, 10).subscribe(System.out::println);
-        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
+        Flux.generate(sink -> {
+            sink.next("hello world1");
+//            sink.complete();
+        }).subscribe(System.out :: println);
+//        Flux.just("Hello","World").subscribe(System.out::println);
+//        Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(System.out::println);
+//        Flux.empty().subscribe(System.out::println);
+//        Flux.range(1, 10).subscribe(System.out::println);
+//        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
     }
     /**
      * 使用 generate()方法生成 Flux 序列
      */
     public static void fluxGenerate(){
+        //生产单个元素即结束
         Flux.generate(sink -> {
             sink.next("hello world1");
             sink.complete();
         }).subscribe(System.out :: println);
 
-        final Random random = new Random();
+        //生产多个元素
+        Random random = new Random();
+        Count count = new Count();
+        Flux.generate(sink -> {
+            sink.next(random.nextInt());
+            count.increase();
+            if (count.count == 99) {
+                sink.complete();
+            }
+        }).subscribe(System.out::println);
+
+
+        Flux.generate(Count::new,(c, sink) -> {
+            c.increase();
+            sink.next(random.nextInt());
+            count.increase();
+            if (c.count == 99) {
+                sink.complete();
+            }
+            return count;
+        }).subscribe(System.out::println);
+
+        //指定
         Flux.generate(ArrayList::new, (list, sink) -> {
             int value = random.nextInt(100);
             list.add(value);
