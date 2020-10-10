ad51e690a504ee6f2677b3d58cc19879bb189b72
luo.yongqian <luo.yongqian@yuexiu-finance.com>
2020-10-10 23:38:45 +0800

    updated

---
+++ temp/src/main/java/com/roboslyq/temp/MainTemp.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2020
+ * FileName: MainTemp
+ * Author:   luo.yongqian
+ * Date:     2020/8/7 8:48
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2020/8/7 8:48      1.0.0               创建
+ */
+package com.roboslyq.temp;
+
+/**
+ *
+ * 〈〉
+ * @author roboslyq
+ * @date 2020/8/7
+ * @since 1.0.0
+ */
+public class MainTemp {
+
+}
--- tu/src/main/java/com/roboslyq/tu/TuFileParse.java
+++ tu/src/main/java/com/roboslyq/tu/TuFileParse.java
@@ -1,41 +1,69 @@
 package com.roboslyq.tu;
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * TU定长文件解析
  * TODO: 目前是单线程，可以优化
+ *
  * @author roboslyq
  * @date 2020/7/24
  * @since 1.0.0
  */
 public class TuFileParse {
-
-    /** 源文件路径: 不需要日期，程序中会自己追加跑批日期*/
+    /**
+     * 源文件路径: 不需要日期，程序中会自己追加跑批日期
+     */
     private static String SRC_FILE_PATH;
-    /** 结果文件保存路径，可以从参数中传递: 不需要日期，程序中会自己追加跑批日期 */
+    /**
+     * 结果文件保存路径，可以从参数中传递: 不需要日期，程序中会自己追加跑批日期
+     */
     private static String RES_FILE_PATH;
-    /** 当前位置*/
-    private static int currIndex = 85;
-    /** 结果文件后缀*/
+    /**
+     * 结果文件后缀
+     */
     private static String RES_FILE_SUFFIX = ".dat";
-    /** OK文件后缀 */
+    /**
+     * OK文件后缀
+     */
     private static String OK_FILE_SUFFIX = ".ok";
-    /** 报表日期*/
+    /**
+     * 报表日期
+     */
     private static String date;
-    /** TU文件名称*/
+    /**
+     * TU文件名称
+     */
     private static String fileName;
-    /** 计数器（行号）*/
+    /**
+     * 计数器（行号）
+     */
     private static AtomicInteger countNum = new AtomicInteger(0);
-    /** 模块名称: 一共5个模块，对应5个不同文件名称*/
+    /**
+     * 模块名称: 一共5个模块，对应5个不同文件名称
+     */
     private static Map<String, String> MODULE_SEQ = new HashMap<>();
-    /** Segments：每1个文件格式一样，由7个组成。segments数组*/
+    /**
+     * Segments：每1个文件格式一样，由7个组成。segments数组
+     */
     private static String[] SEGS = {"NA", "AL", "AD", "PH", "AC", "LM", "RI"};
-    /** 约定，每1个Segments对应一个编号*/
+    /**
+     * 约定，每1个Segments对应一个编号
+     */
     private static Map<String, String> SEGMENT_SEQ = new HashMap<>();
-
-    /** 每一个Segments对应的字段(TU已经规定好格式，不能改 ) */
+    /**
+     * 每一个Segments对应的字段(TU已经规定好格式，不能改 )
+     */
     private static Map<String, String[]> SEGMENT_FIELD = new HashMap<>();
     private static String[] NA_LIST = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"};
     private static String[] AL_LIST = {"03", "05", "06", "07", "08", "09", "10"};
@@ -44,7 +72,9 @@ public class TuFileParse {
     private static String[] AC_LIST = {"01", "02", "04", "05", "07", "08", "09", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "30", "32", "33", "34", "35"};
     private static String[] LM_LIST = {"01", "02"};
     private static String[] RI_LIST = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
-    /**结果*/
+    /**
+     * 结果
+     */
     private static Map<String, List<String>> RESULT_MAP = new HashMap<>();
 
     static {
@@ -81,36 +111,39 @@ public class TuFileParse {
 
     /**
      * 解析文件入口
+     *
      * @param filePath 包括文件路径和具体名称
      * @throws IOException 文件解析异常
      */
-    private void parseFile(String filePath) throws IOException {
+    private void parseFile(String filePath) throws Exception {
         File srcFile = new File(filePath);
-        if(!srcFile.exists()){
+        if (!srcFile.exists()) {
             System.out.println("源文件:" + srcFile + "不在存!!!");
             return;
         }
         BufferedReader fileReader = new BufferedReader(new FileReader(srcFile));
-        String  completeStr = fileReader.readLine();
+        String completeStr = fileReader.readLine();
         // 头部长度
+        // 当前位置: 因为默认文件都包含84个头字符，所以当前位置是85
         int headSize = 85;
-        if(Objects.isNull(completeStr) || completeStr.length() <= headSize){
-            System.out.println("文件数据长度小于85,直接生成结果文件.");
-        }else{
+        if (Objects.isNull(completeStr) || completeStr.length() <= headSize) {
+            System.out.println("文件数据长度小于85,直接生成空结果文件.");
+        } else {
             // 文件中的日期格式为:DDMMYYYY
-            String dateInFile = completeStr.substring(71,75) + completeStr.substring(69,71) + completeStr.substring(67,69);
-            if(!dateInFile.equals(date)){
-                System.out.println("跑批日期：" + date + "与文件日期：" + dateInFile +"不匹配！！！");
+            String dateInFile = completeStr.substring(71, 75) + completeStr.substring(69, 71) + completeStr.substring(67, 69);
+            if (!dateInFile.equals(date)) {
+                System.out.println("跑批日期：" + date + "与文件日期：" + dateInFile + "不匹配！！！");
                 return;
             }
             String srcdata = completeStr.substring(headSize);
             // 按每一笔Loan拆分，得到每一笔Loan的详情，包含7个segments
             String[] resArray = srcdata.split("ES02\\*\\*");
-            Arrays.stream(resArray).forEach(loan -> {
+            for (String loan : resArray) {// 行号，每一笔Loan信息一行
                 int curCountNum = countNum.getAndIncrement();
-                parseLoan(loan, 0, "",curCountNum);
-            });
+                parseLoan(loan, 0, "", curCountNum);
+            }
         }
+        // 生成结果文件
         writeResult(RESULT_MAP);
         System.out.println("parsed the file successfully!");
     }
@@ -118,10 +151,13 @@ public class TuFileParse {
     /**
      * 解析每一笔Loan
      *
-     * @param loan 每一个贷款记录
+     * @param loan        当前行号
+     * @param curIndex    当前位置（使用ES02拆分后的分条记录起始位置，初始化为0）
+     * @param naLineno    当前记录NA对应的编号，例如“NA03”,则对应编号是03
+     * @param curCountNum 当前行号
      */
-    private void parseLoan(String loan, int curIndex, String naLineno,int curCountNum) {
-        if (curIndex == loan.length()){
+    private void parseLoan(String loan, int curIndex, String naLineno, int curCountNum) throws Exception {
+        if (curIndex == loan.length()) {
             return;
         }
         //  the current segment name: NA,AL,AD,PH,AC,LM,RI
@@ -129,10 +165,15 @@ public class TuFileParse {
         int segLength = Integer.parseInt(loan.substring(curIndex + 2, curIndex + 4));
         String segmentLine = loan.substring(curIndex + 4, curIndex + 4 + segLength);
         String[] fields = SEGMENT_FIELD.get(segName);
+        if (Objects.isNull(fields)) {
+            printException(loan, segName, curIndex, curCountNum);
+            throw new Exception("文件解析异常");
+        }
         StringBuilder sb = new StringBuilder();
         //分割符
         String delimiter = "|";
         sb.append(date).append(delimiter).append(curCountNum).append(delimiter);
+        //如果当前处理的segName = "NA",那么重新给naLineno赋值，否则使用之前的旧值。
         if (segName.equals(SEGS[0])) {
             naLineno = segmentLine;
         } else {
@@ -143,6 +184,7 @@ public class TuFileParse {
         curIndex = curIndex + 4 + segLength;
         // 循环处理各种域
         for (String field : fields) {
+            // 表示当前loan已经处理完成，最后剩余的field都为空
             if (curIndex == loan.length()) {
                 //追加空的分割符
                 sb.append(delimiter);
@@ -157,60 +199,77 @@ public class TuFileParse {
                     sb.append(delimiter);
                     curIndex += 4;
                 } else {
-                    String fieldValue = loan.substring(curIndex + 4, curIndex + 4 + fieldLen);
-                    curIndex = curIndex + 4 + fieldLen;
-                    sb.append(fieldValue).append(delimiter);
+                    if (loan.length() < curIndex + 4 + fieldLen) {
+                        printException(loan, segName, curIndex, curCountNum);
+                        throw new Exception("文件解析异常");
+                    } else {
+                        String fieldValue = loan.substring(curIndex + 4, curIndex + 4 + fieldLen);
+                        curIndex = curIndex + 4 + fieldLen;
+                        sb.append(fieldValue).append(delimiter);
+                    }
                 }
             }
         }
         RESULT_MAP.get(segName).add(sb.toString());
         // 递归解析
-        parseLoan(loan, curIndex, naLineno,curCountNum);
+        parseLoan(loan, curIndex, naLineno, curCountNum);
+    }
+
+    /**
+     * 打印异常日志
+     *
+     * @param loan        使用ES02拆分后的记录
+     * @param curIndex    当前位置索引
+     * @param curCountNum 使用ES02拆分后的行号
+     * @param segName     对应segment名称
+     */
+    private void printException(String loan, String segName, int curIndex, int curCountNum) {
+        System.out.println("当前SEGMENT名称：" + segName );
+        System.out.println("源记录：" + loan);
+        System.out.println("当前索引位置：" + curIndex);
+        System.out.println("当前记录在文件中行号(按ES02拆分行号)：" + curCountNum);
     }
 
     /**
      * 将结果写入文件
-     * @param content  结果内容
+     *
+     * @param content 结果内容
      * @throws IOException 写异常
      */
-    private void writeResult(Map<String,List<String>> content) throws IOException {
-        for(String seg :SEGS){
-            List<String> list =  content.get(seg);
-            fileWriter(seg,list);
+    private void writeResult(Map<String, List<String>> content) throws IOException {
+        for (String seg : SEGS) {
+            List<String> list = content.get(seg);
+            fileWriter(seg, list);
         }
     }
 
     /**
      * 初始化
      */
-    private static void init(String[] args){
+    private static void init(String[] args) {
         TuFileParse.fileName = args[0];
         TuFileParse.date = args[1];
         TuFileParse.SRC_FILE_PATH = args[2];
         TuFileParse.RES_FILE_PATH = args[3];
         // 循环清除上一次跑批的结果文件,如果是第1次运行,则不需要删除
-        for(String seg :SEGS){
-            String filePathAndName =RES_FILE_PATH + getResultFileName(seg,RES_FILE_SUFFIX);
-            String okFilePathAndName = RES_FILE_PATH + getResultFileName(seg,OK_FILE_SUFFIX);
-            System.out.println("要删除的文件名：" + filePathAndName);
+        for (String seg : SEGS) {
+            String filePathAndName = RES_FILE_PATH + getResultFileName(seg, RES_FILE_SUFFIX);
+            String okFilePathAndName = RES_FILE_PATH + getResultFileName(seg, OK_FILE_SUFFIX);
             File file = new File(filePathAndName);
             File okFile = new File(okFilePathAndName);
-            if(file.exists()){
-                boolean fileDel = file.delete();
-                boolean okFileDel = okFile.delete();
-                System.out.println("结果文件删除： " + fileDel +", .ok文件删除：" + okFileDel);
-            }else {
-                System.out.println("结果文件" + filePathAndName +"不存在,不需要删除");
+            if (file.exists()) {
+                file.delete();
+                okFile.delete();
             }
-
         }
 
     }
+
     /**
      * 使用字符流
      */
     private void fileWriter(String seg, List<String> content) throws IOException {
-        File file = new File(RES_FILE_PATH + getResultFileName(seg,RES_FILE_SUFFIX));
+        File file = new File(RES_FILE_PATH + getResultFileName(seg, RES_FILE_SUFFIX));
         boolean createNewFileResult = file.createNewFile();
         if (!createNewFileResult) {
             System.out.println("文件创建失败!!");
@@ -225,7 +284,7 @@ public class TuFileParse {
         }
         /* 必须刷新,否则部分数据无法写回文件。  注意,是部分数据,当达到缓冲区存储值,会自动 刷新缓冲区。*/
         out.flush();
-        File okFile = new File(RES_FILE_PATH + getResultFileName(seg,OK_FILE_SUFFIX));
+        File okFile = new File(RES_FILE_PATH + getResultFileName(seg, OK_FILE_SUFFIX));
         boolean okFileRes = okFile.createNewFile();
         if (!okFileRes) {
             System.out.println(".ok 文件创建失败!!");
@@ -234,17 +293,18 @@ public class TuFileParse {
 
     /**
      * 根据seg获取结果文件路径
+     *
      * @param seg seg名称
      * @return 文件名称
      */
-    private static String getResultFileName(String seg,String suffix){
-        return seg + "-" + MODULE_SEQ.get(fileName) + SEGMENT_SEQ.get(seg)  + "-" + date + suffix;
+    private static String getResultFileName(String seg, String suffix) {
+        return seg + "-" + MODULE_SEQ.get(fileName) + SEGMENT_SEQ.get(seg) + "-" + date + suffix;
     }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         int argsNum = 4;
-        if(args.length < argsNum){
+        if (args.length < argsNum) {
             System.out.println("请使用命令`java -jar xxx.jar filename report_date srcdir resdir`来启动。\n" +
                     "例如 `java -jar cif089rp.dat 20200331  D:\\logs\\tu\\20200731\\  D:\\logs\\tu\\res\\20200731\\`");
             System.exit(1);
@@ -259,7 +319,7 @@ public class TuFileParse {
         fileDemo.parseFile(SRC_FILE_PATH + fileName);
         long endTime = System.currentTimeMillis();
         System.out.println("end parse  timestamp (ms): " + endTime);
-        System.out.println("used millis time: " + ( endTime - startTime) + "(ms)");
+        System.out.println("used millis time: " + (endTime - startTime) + "(ms)");
 
     }
 
