4c8de62d4228a1968fb3be9bf94c3bfef5770d50
roboslyq <robosloveu@163.com>
2021-12-21 00:27:44 +0800

    myexcel

---
+++ framework/excel/src/main/java/com/roboslyq/myexcel/MyExcelMain.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2021
+ * FileName: MyExcelMain
+ * Author:   luo.yongqian
+ * Date:     2021/12/21 0:27
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2021/12/21 0:27      1.0.0               创建
+ */
+package com.roboslyq.myexcel;
+
+/**
+ *
+ * 〈〉
+ * @author roboslyq
+ * @date 2021/12/21
+ * @since 1.0.0
+ */
+public class MyExcelMain {
+    public static void main(String[] args) {
+
+    }
+
+}
